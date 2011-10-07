699dfb46913a165b1522111ef092ed65b4b339f5
María Arias de Reyna <marias@emergya.com>
2011-10-07 11:05:36 +0200

    Merging.
    Fixing changes on OpenGIS classes. Now it uses the maven package classes
    instead of our own.

--- trunk/openLS/src/test/java/org/gofleet/openLS/GeoCodingTest.java
+++ trunk/openLS/src/test/java/org/gofleet/openLS/GeoCodingTest.java
@@ -1,5 +1,5 @@
-
 package org.gofleet.openLS;
+
 /*
  * Copyright (C) 2011, Emergya (http://www.emergya.es)
  *
@@ -27,22 +27,24 @@ package org.gofleet.openLS;
  * This exception does not however invalidate any other reasons why the
  * executable file might be covered by the GNU General Public License.
  */
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-import net.opengis.gml.DirectPositionType;
-import net.opengis.gml.PointType;
-import net.opengis.xls.AddressType;
-import net.opengis.xls.GeocodeRequestType;
-import net.opengis.xls.GeocodeResponseListType;
-import net.opengis.xls.GeocodeResponseType;
-import net.opengis.xls.GeocodedAddressType;
-import net.opengis.xls.PositionType;
-import net.opengis.xls.ReverseGeocodeRequestType;
-import net.opengis.xls.ReverseGeocodeResponseType;
-import net.opengis.xls.ReverseGeocodedLocationType;
+import net.opengis.gml.v_3_1_1.DirectPositionType;
+import net.opengis.gml.v_3_1_1.PointType;
+import net.opengis.xls.v_1_2_0.AddressType;
+import net.opengis.xls.v_1_2_0.GeocodeRequestType;
+import net.opengis.xls.v_1_2_0.GeocodeResponseListType;
+import net.opengis.xls.v_1_2_0.GeocodeResponseType;
+import net.opengis.xls.v_1_2_0.GeocodedAddressType;
+import net.opengis.xls.v_1_2_0.PositionType;
+import net.opengis.xls.v_1_2_0.ReverseGeocodeRequestType;
+import net.opengis.xls.v_1_2_0.ReverseGeocodeResponseType;
+import net.opengis.xls.v_1_2_0.ReverseGeocodedLocationType;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -51,7 +53,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GeoCodingTest {
-	
+
 	private GeoCoding geocoding;
 
 	@BeforeClass
@@ -68,89 +70,89 @@ public class GeoCodingTest {
 		geocoding = new GeoCoding();
 	}
 
-	
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	
 	@Test
 	public void testReverseGeocoding() {
 
 		// Lista de valores del punto
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("37.38");
-		list.add("-5.99");
+		ArrayList<Double> list = new ArrayList<Double>();
+		list.add(37.38);
+		list.add(-5.99);
 
 		// Punto que dará contenido al parámetro de entrada
-		PointType point = new net.opengis.xls.gofleet.PointType();
-		DirectPositionType pos = new net.opengis.xls.gofleet.DirectPositionType();
-		pos.setListValue(list);
+		PointType point = new PointType();
+		DirectPositionType pos = new DirectPositionType();
+		pos.setValue(list);
 		point.setPos(pos);
 
 		// Posición de prueba para el parámetro
-		PositionType position = new net.opengis.xls.gofleet.PositionType();
+		PositionType position = new PositionType();
 		position.setPoint(point);
 
 		// Parametros del servicio
-		ReverseGeocodeRequestType param = new net.opengis.xls.gofleet.ReverseGeocodeRequestType();
+		ReverseGeocodeRequestType param = new ReverseGeocodeRequestType();
 		param.setPosition(position);
 		assertNotNull(param);
 
 		// Servicio de geocoding al que se le introduce los parametros
 		ReverseGeocodeResponseType res = geocoding.reverseGeocoding(param);
 		assertNotNull(res);
-		
-		ReverseGeocodedLocationType[] res_array = res.getReverseGeocodedLocationArray();
+
+		List<ReverseGeocodedLocationType> res_array = res
+				.getReverseGeocodedLocation();
 		assertNotNull(res_array);
 		int i = 0;
-		assertTrue(res_array.length > 0);
-		while(i<res_array.length){
-			AddressType addressRes = res_array[i].getAddress();
-			PointType pointRes = res_array[i].getPoint();
+		assertTrue(res_array.size() > 0);
+		for (ReverseGeocodedLocationType locationType : res_array) {
+			AddressType addressRes = locationType.getAddress();
+			PointType pointRes = locationType.getPoint();
 			// Comprobamos que cada parámetro de la dirección no sea nulo
 			assertNotNull(addressRes.getAddressee());
 			assertNotNull(addressRes.getLanguage());
 			assertNotNull(addressRes.getPostalCode());
 			assertNotNull(addressRes.getCountryCode());
-			// Comprobamos que el punto del resultado es el mismo que el del origen
+			// Comprobamos que el punto del resultado es el mismo que el del
+			// origen
 			assertTrue(pointRes.equals(point));
 			i++;
 		}
-		
+
 	}
 
 	@Test
 	public void testGeocoding() {
-		
-		// Comprobamos que pasándole un nombre de una calle nos complete los demas campos
-		
+
+		// Comprobamos que pasándole un nombre de una calle nos complete los
+		// demas campos
+
 		String addressee = "Avenida Innovación";
 		assertNotNull(addressee);
-		
-		AddressType address = new net.opengis.xls.gofleet.AddressType();
+
+		AddressType address = new AddressType();
 		assertNotNull(address);
 		address.setAddressee(addressee);
-		AddressType[] addressArray;
-		addressArray = new AddressType[1];
+		List<AddressType> addressArray = new LinkedList<AddressType>();
 
-		GeocodeRequestType param = new net.opengis.xls.gofleet.GeocodeRequestType();
-		param.setAddressArray(addressArray);
+		GeocodeRequestType param = new GeocodeRequestType();
+		param.setAddress(addressArray);
 		assertNotNull(param);
 
 		GeocodeResponseType res = geocoding.geocoding(param);
 		// Que el resultado no sea nulo
 		assertNotNull(res);
-		GeocodeResponseListType[] arrayRes = res.getGeocodeResponseListArray();
+		List<GeocodeResponseListType> arrayRes = res.getGeocodeResponseList();
 		int i = 0;
 		int j = 0;
 		// Que el array tenga algun dato
-		assertTrue(arrayRes.length>0);
-		while(i<arrayRes.length){
-			GeocodedAddressType[] arrayAddress = arrayRes[i].getGeocodedAddressArray();
-			assertTrue(arrayAddress.length>0);
-			while(j<arrayAddress.length){
-				AddressType paramEval = arrayAddress[j].addNewAddress();
+		assertTrue(arrayRes.size() > 0);
+		for (GeocodeResponseListType list : arrayRes) {
+			List<GeocodedAddressType> arrayAddress = list.getGeocodedAddress();
+			assertTrue(arrayAddress.size() > 0);
+			for (GeocodedAddressType addressType : arrayAddress) {
+				AddressType paramEval = addressType.getAddress();
 				// Comprobamos que cada uno de los parámetros no sea nulo
 				assertNotNull(paramEval.getCountryCode());
 				assertNotNull(paramEval.getLanguage());
