883e084261807bbe2ac8b83f047503b668d7d2ed
Moises Arcos <marcos@emergya.es>
2011-10-13 13:37:07 +0200

    Function of Geocoding direct.

---
+++ trunk/openLS/src/main/database/gofleet_geocoding.sql
@@ -0,0 +1,118 @@
+﻿DROP TYPE IF EXISTS geocoding_address_type CASCADE;
+
+/* Create a specific type to identify an address */
+CREATE TYPE geocoding_address_type AS(
+	street text,
+	munsub text,
+	mun text,
+	subcountry text,
+	country text
+);
+/* Reomove function and types */
+DROP FUNCTION IF EXISTS gofleet_geocoding(geocoding_address_type) CASCADE;
+DROP TYPE IF EXISTS geocoding_type CASCADE;
+
+/* Create a specific type to return the result */
+CREATE TYPE geocoding_type AS(
+	address geocoding_address_type,
+	geometry GEOMETRY
+);
+
+/* Create a function to return the geometry associated to an address */
+CREATE FUNCTION gofleet_geocoding(address_geocoding geocoding_address_type)
+RETURNS SETOF geocoding_type AS
+$BODY$
+DECLARE
+	query TEXT;
+	res_address geocoding_address_type;
+	res geocoding_type;
+	rec RECORD;
+BEGIN
+	/* Create the main query */
+	query := 'SELECT s.name as name_street,
+			ms.name as name_mun_sub,
+			m.name as name_mun,
+			cs.name as name_country_sub, 
+			c.name as name_country,
+			resultado.geometry as geom 
+		FROM street as s,
+			municipality_subdivision as ms,
+			municipality as m,
+			country_subdivision as cs,
+			country as c,
+			(SELECT geometry, id FROM replace_table) as resultado 
+			WHERE st_distance(replaceme.geometry, resultado.geometry) = 0
+			and replaceme.id = resultado.id';
+
+	/* Check the street's name */
+	IF(address_geocoding.street IS NOT NULL) THEN
+		/* Add sql condition */
+		query := query || ' AND s.name ILIKE ';
+		query := query || E'\'';
+		query := query ||'%'||address_geocoding.street||'%';
+		query :=  query || E'\'';
+		/* Replace two values of the main query: the name of the main table and his short name */
+		select replace(query, 'replace_table', 'street') INTO query;
+		select replace(query, 'replaceme', 's') INTO query;
+		RAISE INFO 'Consulta: %', query;
+	END IF;
+	/* Check the municipality subdivision's name */
+	IF(address_geocoding.munsub IS NOT NULL) THEN
+		query := query || ' AND ms.name ILIKE ';
+		query := query || E'\'';
+		query := query ||'%'||address_geocoding.munsub||'%';
+		query :=  query || E'\'';
+		/* Replace two values of the main query: the name of the main table and his short name */
+		select replace(query, 'replace_table', 'municipality_subdivision') INTO query;
+		select replace(query, 'replaceme', 'ms') INTO query;
+		RAISE INFO 'Consulta: %', query;
+	END IF;
+
+	/* Check the municipality's name */
+	IF(address_geocoding.mun IS NOT NULL) THEN
+		query := query || ' AND m.name ILIKE ';
+		query := query || E'\'';
+		query := query ||'%'||address_geocoding.mun||'%';
+		query :=  query || E'\'';
+		/* Replace two values of the main query: the name of the main table and his short name */
+		select replace(query, 'replace_table', 'municipality') INTO query;
+		select replace(query, 'replaceme', 'm') INTO query;
+		RAISE INFO 'Consulta: %', query;
+	END IF;
+	/* Check the country subdivision's name */
+	IF(address_geocoding.subcountry IS NOT NULL) THEN
+		query := query || ' AND cs.name ILIKE ';
+		query := query || E'\'';
+		query := query ||'%'||address_geocoding.subcountry||'%';
+		query :=  query || E'\'';
+		/* Replace two values of the main query: the name of the main table and his short name */
+		select replace(query, 'replace_table', 'country_subdivision') INTO query;
+		select replace(query, 'replaceme', 'cs') INTO query;
+		RAISE INFO 'Consulta: %', query;
+	END IF;
+	/* Check the country subdivision's name */
+	IF(address_geocoding.country IS NOT NULL) THEN
+		query := query || ' AND c.name ILIKE ';
+		query := query || E'\'';
+		query := query ||'%'||address_geocoding.country||'%';
+		query :=  query || E'\'';
+		/* Replace two values of the main query: the name of the main table and his short name */
+		select replace(query, 'replace_table', 'country') INTO query;
+		select replace(query, 'replaceme', 'c') INTO query;
+		RAISE INFO 'Consulta: %', query;
+	END IF;	
+	
+	FOR rec IN EXECUTE(query) LOOP
+	/* Add the results to the type to return */
+		res_address.street := rec.name_street;
+		res_address.munsub := rec.name_mun_sub;
+		res_address.mun := rec.name_mun;
+		res_address.subcountry := rec.name_country_sub;
+		res_address.country := rec.name_country;
+		res.address := res_address;
+		res.geometry := rec.geom;
+		RETURN NEXT res;
+	END LOOP;	
+END;
+$BODY$
+LANGUAGE 'plpgsql';
