ed6eaedc47bab8c1199599ec2aac737199ea0dc3
María Arias de Reyna <marias@emergya.com>
2011-10-17 10:52:08 +0200

    Environment variables

--- trunk/openLS/src/main/database/customloads/loadNavTeq.sh
+++ trunk/openLS/src/main/database/customloads/loadNavTeq.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+#Configuration:
+#Customize the following variables with your own environment:
+DATABASE="gofleetls"
+USERNAME="gofleetls"
+HOST="127.0.0.1"
+PORT="5432"
 SHP_PATH="/var/svn/openls-seneca/trunk/NavteqQ22011_Andalucia/K2AM11100EK2000AAAEX/";
-HERE=pwd;
 
+
+#Shouldn't touch the following code
+HERE=pwd;
 cd $SHP_PATH;
 
 for f in *.shp
 do
-	shp2pgsql -c -s 4326 -i -I -N skip -D $f | psql -U gofleetls gofleetls
+	shp2pgsql -c -s 4326 -i -I -N skip -D $f | psql -U $USERNAME $DATABASE -h $HOST -p $PORT
 done;
 
 cd $HERE;
