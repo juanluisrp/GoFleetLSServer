f2c68733af2c05e596a48a9c20618bf27bae6c0c
María Arias de Reyna <marias@emergya.com>
2011-12-12 11:39:51 +0100

    Routing by levels

--- src/main/database/customloads/loadTeleAtlas.sh
+++ src/main/database/customloads/loadTeleAtlas.sh
@@ -11,136 +11,290 @@ echo "Now, to important stuff:"
 
 #Configuration:
 #Customize the following variables with your own environment:
-DATABASE="gofleetls"
-USERNAME="gofleetls"
+DATABASE="gofleet-madrid"
+USERNAME="gofleet"
 HOST="postgresql-apps"
 PORT="5432"
 
 NOW=$(date +%s)
 
-echo "Generating custom tables"
+PSQL="psql -U $USERNAME -h $HOST -p $PORT $DATABASE "
 
-$PSQL -c "CREATE SEQUENCE routing$NOW START 1;"
+echo "Preparing helper views"
 
-$PSQL -c "INSERT INTO routing_$NOW
-(SELECT 
-        nextval('routing_id_seq$NOW') as id,
-        s.link_id as gid,
-	CASE (SELECT c.dir_travel FROM streets as c 
-			where s.link_id = c.link_id limit 1)
-		WHEN 'B' THEN st_length(s.the_geom)::double precision
-		WHEN 'F' THEN st_length(s.the_geom)::double precision
-		ELSE 'infinity'::double precision
-        END as cost,
-	CASE (SELECT dir_travel FROM streets 
-			where link_id = streets.link_id limit 1) 
-		WHEN 'B' THEN st_length(s.the_geom)::double precision
-		WHEN 'T' THEN st_length(s.the_geom)::double precision
-		ELSE 'infinity'::double precision
-        END as reverse_cost,
-        -1::integer as source,
-        -1::integer as target,
-        st_X(st_startpoint(s.the_geom::geometry))::double precision as x1,
-        st_Y(st_startpoint(s.the_geom::geometry))::double precision as y1,
-        st_X(st_endpoint(s.the_geom::geometry))::double precision as x2,
-        st_Y(st_endpoint(s.the_geom::geometry))::double precision as y2,
-        'infinity'::double precision as to_cost,
-	man_linkid::text as rule,
-	s.the_geom as geometry
-FROM 
-	(SELECT gen.the_geom, gen.link_id, cond.cond_id FROM
-		(select st_linefrommultipoint(st_collect(t.the_geom)) as the_geom, 
-			t.link_id 
+$PSQL -c "create or replace view
+                network_00
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_00 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_01
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_01 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_02
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_02 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_03
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_03 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_04
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_04 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_05
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_05 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_06
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_06 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view
+                network_07
+        as
+                select n.*
+                from
+                        t_network as n,
+                        t_streets_07 as s
+                where
+                        st_contains(s.the_geom, n.the_geom);"
+$PSQL -c "create or replace view 
+		network_08
+	as
+		select n.*
+		from
+		 	t_network as n,
+		 	t_streets_08 as s
+		where 
+			st_contains(s.the_geom, n.the_geom);"
+
+echo "Processing separated types of roads"
+
+for i in 0 1 2 3 4 5 6 7 8
+do
+
+	$PSQL -c "DROP VIEW routing_restrictions_view_0$i;"
+
+	$PSQL -c "CREATE OR REPLACE VIEW routing_restrictions_view_0$i AS 
+ 	SELECT 	
+		network.id, 
+		network.gid, 
+		network.the_geom, 
+		network.x1, 
+		network.y1, 
+		network.x2, 
+		network.y2, 
+		target.id::text AS rule, 
+		network.f_jnctid_fk AS source, 
+		network.t_jnctid_fk AS target, 
+		'Infinity'::double precision AS to_cost, 
+        	CASE network.oneway
+        	    WHEN 'N'::text THEN 'Infinity'::double precision
+        	    WHEN 'TF'::text THEN 'Infinity'::double precision
+        	    ELSE network.minutes
+        	END AS cost, 
+        	CASE network.oneway
+        	    WHEN 'N'::text THEN 'Infinity'::double precision
+        	    WHEN 'FT'::text THEN 'Infinity'::double precision
+        	    ELSE network.minutes
+        	END AS reverse_cost,
+		network.name as name,
+		network.namelc as namelc,
+		network.nametyp as nametyp,
+		network.rtetyp as rtetyp
+   	FROM 	
+		network_0$i network, 
+		t_network target, 
+		t_junctions junction, 
+		t_maneuvers man, 
+		t_maneuvers_path_index man_path
+	WHERE 
+		network.f_jnctid_fk = junction.gid 
+		AND target.t_jnctid_fk = junction.gid 
+		AND man.jnctid = junction.id 
+		AND man_path.id = man.id 
+		AND junction.feattyp = 4120;"
+
+	$PSQL -c "DROP VIEW routing_norestrictions_view_0$i;"
+
+	$PSQL -c "CREATE OR REPLACE VIEW routing_norestrictions_view_0$i AS
+        SELECT
+                network.id,
+                network.gid,
+                network.the_geom,
+                network.x1,
+                network.y1,
+                network.x2,
+                network.y2,
+                ''::text AS rule,
+                network.f_jnctid_fk AS source,
+                network.t_jnctid_fk AS target,
+                'Infinity'::double precision AS to_cost,
+                CASE network.oneway
+                    WHEN 'N'::text THEN 'Infinity'::double precision
+                    WHEN 'TF'::text THEN 'Infinity'::double precision
+                    ELSE network.minutes
+                END AS cost,
+                CASE network.oneway
+                    WHEN 'N'::text THEN 'Infinity'::double precision
+                    WHEN 'FT'::text THEN 'Infinity'::double precision
+                    ELSE network.minutes
+                END AS reverse_cost,
+                network.name as name,
+                network.namelc as namelc,
+                network.nametyp as nametyp,
+                network.rtetyp as rtetyp
+        FROM
+                network_0$i network
+        WHERE
+		not (exists (select 1 from routing_restrictions_view_0$i restr
+				where restr.source = network.f_jnctid_fk and restr.target = network.t_jnctid_fk));"
+
+	$PSQL -c "CREATE TABLE routing_""0$i""_$NOW
+	(
+	  id serial,
+	  former_id numeric(15,0),
+	  gid integer,
+	  the_geom geometry,
+	  x1 double precision,
+	  y1 double precision,
+	  x2 double precision,
+	  y2 double precision,
+	  rule text,
+	  source integer,
+	  target integer,
+	  to_cost double precision,
+	  cost double precision,
+	  reverse_cost double precision,
+	  name text DEFAULT 'unknown'::text
+	);"
+
+	echo "Generate gist"
+	
+	$PSQL -c "CREATE INDEX routing_gist_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING gist
+	  (the_geom );"
+	
+	$PSQL -c "ALTER TABLE routing_""0$i""_$NOW CLUSTER ON routing_gist_idx_$NOW$i;"
+	
+	echo "Generate idx"
+
+	$PSQL -c "CREATE INDEX routing_id_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING btree
+	  (id );"
+	
+	echo "Generate name id"
+
+	$PSQL -c "CREATE INDEX routing_name_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING btree
+	  (name );"
+	
+	echo "Generate rule id"
+	
+	$PSQL -c "CREATE INDEX routing_rule_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING btree
+	  (rule );"
+	
+	echo "Generate source id"	
+
+	$PSQL -c "CREATE INDEX routing_source_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING btree
+	  (source );"
+
+	echo "Generate target id"
+	
+	$PSQL -c "CREATE INDEX routing_target_idx_$NOW$i
+	  ON routing_""0$i""_$NOW
+	  USING btree
+	  (target);"
+	
+	echo "Generating routing rows"
+	
+	echo "Generating restrictions rows"
+	
+	$PSQL -c "INSERT INTO routing_""0$i""_$NOW
+		(SELECT
+		  null,
+		  id,
+		  gid,
+		  the_geom,
+		  x1,
+		  y1,
+		  x2,
+		  y2,
+		  rule,
+		  source,
+		  target,
+		  to_cost,
+		  cost,
+		  reverse_cost,
+		  name 
 		from 
-		(select the_geom, link_id 
-			from zlevels order by link_id, point_num) as t
-			group by t.link_id) as gen,
-		cdms as cond
-		where gen.link_id = cond.link_id
-		and cond.cond_type = 7) as s,
-	rdms
-WHERE rdms.cond_id = s.cond_id
-AND rdms.link_id = s.link_id);"
+			routing_restrictions_view_0$i);"
+		
+	echo "Generating no restriction rows"
+	
+	$PSQL -c "INSERT INTO routing_""0$i""_$NOW
+        (SELECT 
+	  null,
+	  id,
+          gid,
+          the_geom,
+          x1,
+          y1,
+          x2,
+          y2,
+          rule,
+          source,
+          target,
+          to_cost,
+          cost,
+          reverse_cost,
+          name             
+        from 
+                routing_norestrictions_view_0$i);"
 
-$PSQL -c "INSERT INTO routing_$NOW  
-(SELECT
-        nextval('routing_id_seq$NOW') as id,
-        s.link_id as gid, 
-	'infinity'::double precision as cost,
-	'infinity'::double precision as reverse_cost,
-        -1::integer as source,
-        -1::integer as target,
-        st_X(st_startpoint(s.the_geom::geometry))::double precision as x1,
-        st_Y(st_startpoint(s.the_geom::geometry))::double precision as y1,
-        st_X(st_endpoint(s.the_geom::geometry))::double precision as x2,
-        st_Y(st_endpoint(s.the_geom::geometry))::double precision as y2,
-        'infinity'::double precision as to_cost,
-	man_linkid::text as rule,
-        s.the_geom as geometry
-FROM 
-	(SELECT gen.the_geom, gen.link_id, cond.cond_id FROM
-		(select st_linefrommultipoint(st_collect(t.the_geom)) as the_geom, 
-			t.link_id 
-		from 
-		(select the_geom, link_id 
-			from zlevels order by link_id, point_num) as t
-			group by t.link_id) as gen,
-		cdms as cond
-		where gen.link_id = cond.link_id
-		and cond.cond_type = 4) as s,
-	rdms
-WHERE rdms.cond_id = s.cond_id
-AND rdms.link_id = s.link_id);"
-
-$PSQL -c "INSERT INTO routing_$NOW  
-(SELECT 
-        nextval('routing_id_seq$NOW') as id,
-        s.link_id as gid,
-	CASE (SELECT c.dir_travel FROM streets as c where s.link_id = c.link_id limit 1)
-		WHEN 'B' THEN st_length(s.the_geom)::double precision
-		WHEN 'F' THEN st_length(s.the_geom)::double precision
-		ELSE 'infinity'::double precision
-        END as cost,
-	CASE (SELECT dir_travel FROM streets where link_id = streets.link_id limit 1) 
-		WHEN 'B' THEN st_length(s.the_geom)::double precision
-		WHEN 'T' THEN st_length(s.the_geom)::double precision
-		ELSE 'infinity'::double precision
-        END as reverse_cost,
-        -2::integer as source,
-        -2::integer as target,
-        st_X(st_startpoint(s.the_geom::geometry))::double precision as x1,
-        st_Y(st_startpoint(s.the_geom::geometry))::double precision as y1,
-        st_X(st_endpoint(s.the_geom::geometry))::double precision as x2,
-        st_Y(st_endpoint(s.the_geom::geometry))::double precision as y2,
-        'infinity'::double precision as to_cost,
-	''::text as rule,
-        st_geometryn(s.the_geom,1) as geometry
-FROM streets s
-WHERE NOT EXISTS(SELECT 1 FROM routing_$NOW r WHERE s.link_id = r.gid));"
-
-$PSQL -c "SELECT assign_vertex_id('routing_$NOW', 0.000001, 'geometry', 'id');"
-
-$PSQL -c "INSERT INTO routing_$NOW
-(SELECT
-        nextval('routing_id_seq$NOW') as id,
-        source.gid as gid,
-        source.cost as cost,
-        source.reverse_cost as reverse_cost,
-        source.source as source,
-        source.target as target,
-        source.x1 as x1,
-        source.y1 as y1,
-        source.x2 as x2,
-        source.y2 as y2,
-        'infinity'::double precision as to_cost,
-        target.gid::text as rule,
-        source.geometry as geometry
-FROM 
-	(SELECT r.*, z.z_level FROM routing r, zlevels z where r.gid = z.link_id order by z.point_num = (select max(point_num) from zlevels as l where l.link_id = z.link_id)) as source, 
-	(SELECT r.source, z.z_level FROM routing r, zlevels z where r.gid = z.link_id and z.point_num = (select max(point_num) from zlevels as l where l.link_id = z.link_id)) as target
-WHERE source.target = target.source
-AND source.z_level <> target.z_level);"
-
-$PSQL -c "DELETE FROM routing_$NOW r WHERE EXISTS (SELECT 1 FROM routing s WHERE s.gid = r.gid AND s.id <> r.id AND r.rule LIKE '');" 
+done
 
 echo "Importation successfull."
